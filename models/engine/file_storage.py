fec8e6e56261f76052ff080ae6bda3f10474e89b
Pablo Andrés Sánchez <pabsanchez@gmail.com>
2019-11-14 00:36:39 -0500

    adding modules

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,8 +4,16 @@ Contains File Storage
 """
 import json
 from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.state import State
+from models.user import User
+from models.review import Review
 
-classes = {'BaseModel': BaseModel}
+
+classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 class FileStorage:
